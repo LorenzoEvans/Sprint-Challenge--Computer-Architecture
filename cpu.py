a6bb1a518eb4d94d1d97e84862a0e4dcdfbccce5
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-09-03 15:37:40 -0400

    Adding internal class objects.

--- cpu.py
+++ cpu.py
@@ -5,3 +5,12 @@ class CPU:
 		self.ram = [0] * 256
 		self.pc = 0
 		self.registers = [0] * 8
+		self.PRN = 0b01000111
+		self.ADD = 0b10100000
+		self.SUB = 0b10100001
+		self.MUL = 0b10100010
+		self.LDI = 0b10000010
+		self.HLT = 0b00000001
+		self.DIV = 0b10100011
+		self.MOD = 0b10100100
+		self.running = True
